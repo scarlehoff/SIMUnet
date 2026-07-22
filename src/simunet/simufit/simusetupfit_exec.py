74bdbcb193684eab6280db39e6225dfeda6699c0
juacrumar <juacrumar@lairen.eu>
2026-07-22 14:25:08 +0200

    override level 0 data generation so it works with fixed predictions

--- src/simunet/simufit/simusetupfit_exec.py
+++ src/simunet/simufit/simusetupfit_exec.py
@@ -22,6 +22,22 @@ class SimuSetupConfig(SIMUConfig, SetupFitConfig):
         """Skip the fktable hasher as not all data will have an fktable."""
         pass
 
+    @classmethod
+    def from_yaml(cls, o, *args, **kwargs):
+        """For closure test, the filtering for the level 0 closure test is a chain
+        of internal imports, leading to ``dataset_t0_predictions``.
+        In order to perform closuretests with our own version of ``dataset_t0_predictions``
+        (with fixed predictions)
+        it is necessary to substitute the validphys internal function.
+        """
+        tmp = super().from_yaml(o, *args, **kwargs)
+        if tmp.input_params.get("closuretest") is not None:
+            from simunet.n3fit_providers import dataset_t0_predictions
+            from validphys import covmats
+
+            covmats.dataset_t0_predictions = dataset_t0_predictions
+        return tmp
+
 
 class SimunetSetupfitApp(SetupFitApp):
     environment_class = SimuSetupEnvironment
